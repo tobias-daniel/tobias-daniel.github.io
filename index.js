b8da965c72160d5eff4630c3f50b4b0fa5b65279
tobias-daniel <jtdacosta123@gmail.com>
2023-03-30 17:39:44 -0300

    hola

---
+++ index.js
@@ -0,0 +1,9 @@
+function opcion1(){
+    window.alert("HABILIDADES: manejo vehiculos, hago pack office, tengo muy buena memoria, soy bueno como metematicas y fisica, hago paginas web, capacidad analitica");
+}
+function opcion2(){
+    window.alert('IDIOMAS: español, ingles, albano, frances, portugues');
+}
+function opcion3(){
+    window.alert('INTERESES: me gustan las plantas, todo lo que sea referido a tecnologia tambien, lectura grupal de ingles, paseos turisticos');
+}
